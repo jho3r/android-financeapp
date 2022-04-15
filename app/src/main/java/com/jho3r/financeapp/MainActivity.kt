be2b6525700690fa0b93e85ae65efc91b70820c9
jho3r <jhoer.perez@gmail.com>
2022-04-14 23:20:08 -0500

    First basic activity created for MVP

---
+++ app/src/main/java/com/jho3r/financeapp/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.jho3r.financeapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.action_settings -> {
+            // go to config activity
+            val intent = Intent(this, ConfigActivity::class.java)
+            startActivity(intent)
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
+    }
+}
